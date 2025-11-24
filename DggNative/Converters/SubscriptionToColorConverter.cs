63211bc4e67d2bb3d4ef04b80691f20549650f17
Jack Lualdi <jacklualdi@gmail.com>
2025-11-24 13:13:26 -0500

    feat: add subscription tier colors

---
+++ DggNative/Converters/SubscriptionToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Controls;
+using Avalonia.Media;
+using DggNative.Models;
+
+namespace DggNative.Converters;
+
+public class SubscriptionToColorConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        object? brush = null;
+        if (value is Subscription sub)
+            Application.Current?.TryFindResource($"Tier{sub.Tier}Brush", out brush);
+        return brush ?? Brushes.White;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
