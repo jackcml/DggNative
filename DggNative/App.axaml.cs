31039fafa678351e0af3aadc0c7e7448737e578a
Jack Lualdi <jacklualdi@gmail.com>
2026-04-21 16:58:32 -0400

    chore: migrate from Avalonia 11 to 12
    - Bump Avalonia packages to 12.0.1, Diagnostics to 11.3.14, CommunityToolkit.Mvvm to 8.4.2
    - Remove BindingPlugins workaround (API removed in Avalonia 12)
    - Rename TextBox.Watermark to PlaceholderText

--- DggNative/App.axaml.cs
+++ DggNative/App.axaml.cs
@@ -1,8 +1,6 @@
 using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
 using DggNative.Services;
 using DggNative.ViewModels;
@@ -21,10 +19,6 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-
             var wsurl = Environment.GetEnvironmentVariable("wsurl");
             if (string.IsNullOrEmpty(wsurl))
             {
@@ -38,18 +32,5 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
     }
 }
